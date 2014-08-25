e1b3399afbf4a861458327e9735e3dc4e2af464a
Richard Lea <chigix@zoho.com>
2014-08-25 23:55:47 +0800

    FIX resource required to be from roadmap.
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- src/Annotation/RequireAnnotation.php
+++ src/Annotation/RequireAnnotation.php
@@ -19,8 +19,8 @@
 namespace Chigi\Chiji\Annotation;
 
 use Chigi\Chiji\Exception\ResourceNotFoundException;
-use Chigi\Chiji\File\AbstractResourceFile;
 use Chigi\Chiji\File\RequiresMapInterface;
+use Chigi\Chiji\Project\Project;
 use Chigi\Chiji\Util\PathHelper;
 use Chigi\Chiji\Util\ResourcesManager;
 
@@ -33,12 +33,6 @@ use Chigi\Chiji\Util\ResourcesManager;
 class RequireAnnotation extends AbstractAnnotation {
 
     /**
-     *
-     * @var AbstractResourceFile
-     */
-    private $requireResource = null;
-
-    /**
      * Parse the path param in the require annotation to an Resource File Object.
      * @param string $param_str The String as params following the command name
      * @throws ResourceNotFoundException
@@ -46,21 +40,19 @@ class RequireAnnotation extends AbstractAnnotation {
     public function parse($param_str) {
         $real_path = PathHelper::searchRealPath($this->getScope()->getRealPath(), trim($param_str));
         if (file_exists($real_path)) {
-            $this->requireResource = ResourcesManager::getResourceByPath($real_path);
+            if (is_null($require_resource = ResourcesManager::getResourceByPath($real_path))) {
+                $road = Project::getRegistered()->getMatchRoad($real_path);
+                if (is_null($road)) {
+                    throw new ResourceNotFoundException("NO roadmap found for the resource:$real_path");
+                }
+                $require_resource = ResourcesManager::getResourceByPath($real_path);
+            }
             if ($this->getScope() instanceof RequiresMapInterface) {
-                $this->getScope()->getRequires()->addResource($this->getResource());
+                $this->getScope()->getRequires()->addResource($require_resource);
             }
         } else {
             throw new ResourceNotFoundException("The file $param_str NOT FOUND FROM " . $real_path->getRealPath());
         }
-    }
-
-    /**
-     * 
-     * @return AbstractResourceFile
-     */
-    public function getResource() {
-        return $this->requireResource;
     }
 
 }
